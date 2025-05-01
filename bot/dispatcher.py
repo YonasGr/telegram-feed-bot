2b8a9a5674e8b29f3e6e7529962d87323dca3675
Yonas Girma <yonasgirma222@gmail.com>
2025-05-01 09:23:03 -0400

    Update dispatcher.py

--- bot/dispatcher.py
+++ bot/dispatcher.py
@@ -1,3 +1,6 @@
 from aiogram import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from .handlers import register_handlers 
+from .handlers import register_handlers
+
+dp = Dispatcher(storage=MemoryStorage())
+register_handlers(dp)
